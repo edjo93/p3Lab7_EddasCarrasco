0d7cac38d2482f3cac29fbf02e368b31584bf4d6
edjo93 <eddas.carrasco@unitec.edu>
2019-08-30 13:16:55 -0600

    consola y logger

---
+++ Consola.cpp
@@ -0,0 +1,10 @@
+#ifndef CONSOLA_CPP
+#define CONSOLA_CPP
+	class Consola{
+		private:
+			string usuario;
+			Logger*logger;
+		public:
+			
+	};
+#endif
---
+++ Logger.cpp
@@ -0,0 +1,14 @@
+#ifndef LOGGER_CPP
+#define LOGGER_CPP
+	class Logger{
+		private:
+			string nombreArchivo;
+			string usuario;
+			int numLog;
+			vector<Log*>logs;
+			//opcional writer de ofstream
+			
+			
+		
+	};
+#endif
